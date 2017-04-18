68aa27987e999cd8eb7be1c0d97d011f0f386aef
sretundijr <sretundijr@gmail.com>
2017-04-17 23:27:57 -0600

    app state saved in similar artist

--- js/app.js
+++ js/app.js
@@ -2,27 +2,37 @@
 
 var TASTE_DIVE_BASE_URL = 'https://tastedive.com/api/similar?callback=?';
 
+var similarArtists = [];
 
-function getDataFromApi(searchTerm, callback) {
+
+function getDataFromTasteDive(searchTerm, callback) {
   var query = {
     q: searchTerm,
+    type: "music",
+    info: 1,
     limit: 10,
-    key: "267313-capstone-T3SYRQMZ"
+    key: "267313-capstone-T3SYRQMZ" //you see nothing
   }
     // console.log($.getJSON(TASTE_DIVE_BASE_URL, query, callback));
     $.getJSON(TASTE_DIVE_BASE_URL, query, callback);
 }
 
-function displayApiResults(data) {
+function tasteDiveResults(data){
+    data.Similar.Results.forEach(function(item, index){
+        similarArtists.push(item);
+    })
+    showResults(similarArtists);
+}
 
-  console.log(data);
+function showResults(similarArtists){
+    console.log(similarArtists);
 }
 
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
     var query = $(this).find('.js-query').val();
-    getDataFromApi(query, displayApiResults);
+    getDataFromTasteDive(query, tasteDiveResults);
   });
 }
 
