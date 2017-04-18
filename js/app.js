e1135ecc2bf911f889ed94804f43b04cc00bc69c
sretundijr <sretundijr@gmail.com>
2017-04-18 00:28:45 -0600

    returning spotify results

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@
 
 var TASTE_DIVE_BASE_URL = 'https://tastedive.com/api/similar?callback=?';
 
+var SPOTIFY_BASE_URL = "https://api.spotify.com/v1/search";
+
 var similarArtists = [];
 
+var spotifyData = [];
 
 function getDataFromTasteDive(searchTerm, callback) {
   var query = {
     q: searchTerm,
-    type: "music",
+    type: "artist",
     info: 1,
     limit: 10,
     key: "267313-capstone-T3SYRQMZ" //you see nothing
@@ -17,15 +20,37 @@ function getDataFromTasteDive(searchTerm, callback) {
     $.getJSON(TASTE_DIVE_BASE_URL, query, callback);
 }
 
+function getDataFromSpotify(band, callback){
+    var query = {
+        q: band,
+        type: "artist",
+        limit: 1
+    }
+    // console.log("here");
+    console.log($.getJSON(SPOTIFY_BASE_URL, query, callback));
+    $.getJSON(SPOTIFY_BASE_URL, query, callback);
+}
+
+function spotifyResults(data){
+    // data.
+    // alert("here");
+    console.log(data);
+}
+
 function tasteDiveResults(data){
     data.Similar.Results.forEach(function(item, index){
         similarArtists.push(item);
+        // console.log(similarArtists[index].Name)
+        // getDataFromSpotify(similarArtists[index].Name, spotifyResults);
     })
     showResults(similarArtists);
 }
 
 function showResults(similarArtists){
-    console.log(similarArtists);
+    // console.log(similarArtists);
+    similarArtists.forEach(function(item, index){
+        getDataFromSpotify(item.Name, spotifyResults);
+    })
 }
 
 function watchSubmit() {
