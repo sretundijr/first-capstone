5a844f5217b711bb75357462280c6a0319077188
sretundijr <sretundijr@gmail.com>
2017-04-23 13:36:26 -0600

    made seeveral changes, added html tamplate and render function.  added screen shot of returned results rendering.  styles added

--- js/app.js
+++ js/app.js
@@ -4,23 +4,30 @@ var TASTE_DIVE_BASE_URL = 'https://tastedive.com/api/similar?callback=?';
 
 var SPOTIFY_BASE_URL = "https://api.spotify.com/v1/search";
 
-var similarArtists = [];
-
-var spotifyData = [];
+var state = {
+    similarArtists: [],
+    spotifyData: [],
+    hasArtists: function () {
+        return this.similarArtists.length > 0;
+    },
+    hasSpotifyData: function () {
+        return this.spotifyData.length > 0;
+    }
+};
 
 function getDataFromTasteDive(searchTerm, callback) {
-  var query = {
-    q: searchTerm,
-    type: "artist",
-    info: 1,
-    limit: 10,
-    key: "267313-capstone-T3SYRQMZ" //you see nothing
-  }
+    var query = {
+        q: searchTerm,
+        type: "artist",
+        info: 1,
+        limit: 10,
+        key: "267313-capstone-CI6ZV80O" //you see nothing
+    }
     // console.log($.getJSON(TASTE_DIVE_BASE_URL, query, callback));
     $.getJSON(TASTE_DIVE_BASE_URL, query, callback);
 }
 
-function getDataFromSpotify(band, callback){
+function getDataFromSpotify(band, callback) {
     var query = {
         q: band,
         type: "artist",
@@ -31,34 +38,80 @@ function getDataFromSpotify(band, callback){
     $.getJSON(SPOTIFY_BASE_URL, query, callback);
 }
 
-function spotifyResults(data){
-    spotifyData.push(data);
+function spotifyResults(data) {
+    state.spotifyData.push(data);
     // console.log(data.artists.items[0].name);
-    // console.log(spotifyData);
+    //console.log(state.spotifyData);
+    render();
 }
 
-function tasteDiveResults(data){
-    data.Similar.Results.forEach(function(item, index){
-        similarArtists.push(item);
+function tasteDiveResults(data) {
+    console.log(data);
+    data.Similar.Results.forEach(function (item, index) {
+        state.similarArtists.push(item);
         // console.log(similarArtists[index].Name)
         // getDataFromSpotify(similarArtists[index].Name, spotifyResults);
     })
-    sendResultsToSpotify(similarArtists);
+    render();
 }
 
-function sendResultsToSpotify(similarArtists){
+function sendResultsToSpotify(similarArtists) {
     // console.log(similarArtists);
-    similarArtists.forEach(function(item, index){
+    similarArtists.forEach(function (item, index) {
         getDataFromSpotify(item.Name, spotifyResults);
     })
+    // render();
 }
+// (getTasteDiveResults, sendResultsToSpotify).value => {similarArtists[]: spotify}
+// getTasteDiveResults.then(sendResultsToSpotify).resolve()
+function render() {
+    if (state.hasArtists()) {
+        var artists = state.similarArtists.map(function (item) {
+            return "<li>" + item.Name + "</li>";
+        });
+        $(".artists").html(artists.join(""));
+    } else {
+        console.log('here');
+        getDataFromTasteDive(state.query, tasteDiveResults);
+    }
 
+    if (state.hasSpotifyData()) {
+        var images = state.spotifyData.map(function (item) {
+            return "<img class='thumbnail' src='" + item.artists.items[0].images[0].url + "'/>";
+        });
+        $(".images").html(images.join(""));
+    } else if (state.similarArtists) {
+        sendResultsToSpotify(state.similarArtists);
+    }
+
+}
 function watchSubmit() {
-  $('.js-search-form').submit(function(e) {
-    e.preventDefault();
-    var query = $(this).find('.js-query').val();
-    getDataFromTasteDive(query, tasteDiveResults);
-  });
+    $('.js-search-form').submit(function (e) {
+        e.preventDefault();
+        var query = $(this).find('.js-query').val();
+        state.query = query;
+        console.log(state.query);
+        render();
+    });
 }
 
-$(function(){watchSubmit();});
+function htmlTemplate(state) {
+    var html = '<div class="row main-container">' +
+        '<div class="col-8">' +
+        '<div class="artist-container">' +
+        '<h3>' + state.similarArtists[0] + '</h3>' +
+        '<p>description</p>' +
+        '<button>play album</button>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-4">' +
+        '<div class="img-container">' +
+        '<img class="artist-img" src="http://placeimg.com/640/480/any" alt="placeholder">' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+
+    return html;
+}
+
+$(function () { watchSubmit(); });
