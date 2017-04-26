5e10867a02c56af3cdb45d5f57d38f3a8331d69d
sretundijr <sretundijr@gmail.com>
2017-04-25 23:35:11 -0600

    added promises to solve sync issues and modified existing functions

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var SPOTIFY_BASE_URL = "https://api.spotify.com/v1/search";
 
 var state = {
     similarArtists: [],
-    spotifyData: [],
+    // spotifyData: [],
     hasArtists: function () {
         return this.similarArtists.length > 0;
     },
@@ -16,7 +16,7 @@ var state = {
     }
 };
 
-function getDataFromTasteDive(searchTerm, callback) {
+function getDataFromTasteDive(searchTerm) {
     var query = {
         q: searchTerm,
         type: "artist",
@@ -24,80 +24,94 @@ function getDataFromTasteDive(searchTerm, callback) {
         limit: 10,
         key: "268196-Similara-AYQO3GFI" //you see nothing
     }
-    $.getJSON(TASTE_DIVE_BASE_URL, query, callback);
+    return Promise.resolve($.getJSON(TASTE_DIVE_BASE_URL, query));
 }
 
-function getDataFromSpotify(band, callback) {
+function getDataFromSpotify(band) {
     var query = {
         q: band,
         type: "artist",
         limit: 1
     }
-    $.getJSON(SPOTIFY_BASE_URL, query, callback);
+    return Promise.resolve($.getJSON(SPOTIFY_BASE_URL, query));
 }
 
 function spotifyResults(data) {
-    state.spotifyData.push(data);
-    renderArtistImage();
+    state.similarArtists.forEach(function (art, index) {
+        //TODO: Point to album and thumbinal
+        art.Thumbnail = data[index].artists.items[0].images;
+        art.ArtistId = data[index].artists.items[0].id;
+        // console.debug(data[index].artists.items[0].images);
+
+    })
+    //state.spotifyData.push(data);
+    // renderArtistImages();
+    renderSimilarArtists();
 }
 
 function tasteDiveResults(data) {
-    console.log(data);
+    // console.log(data);
     data.Similar.Results.forEach(function (item, index) {
         state.similarArtists.push(item);
     })
-    renderSimilarArtist();
+    renderSimilarArtists();
+    sendResultsToSpotify(state.similarArtists)
 }
 
-function sendResultsToSpotify(similarArtists) {
-    similarArtists.forEach(function (item, index) {
-        getDataFromSpotify(item.Name, spotifyResults);
+function sendResultsToSpotify(data) {
+    // console.debug(data);
+    var promises = data.map(function (item, index) {
+        return getDataFromSpotify(item.Name);
     })
+    Promise.all(promises).then(spotifyResults);
 }
 
-function renderSimilarArtist() {
+function renderSimilarArtists() {
 
     if (state.hasArtists()) {
         var artists = state.similarArtists.map(function (item, index) {
             return htmlTemplate(item, index)
         });
+        // console.debug(artists.join(""));
         $(".js-results").html(artists.join(""));
-        sendResultsToSpotify(state.similarArtists);
+
     } else {
-        getDataFromTasteDive(state.query, tasteDiveResults);
+        getDataFromTasteDive(state.query).then(tasteDiveResults);
     }
 }
 
-function renderArtistImage() {
-    state.spotifyData.forEach(function (item, index) {
-        var html = htmlArtistImg(item, index);
-        $(".img-container").eq(index).html(html);
-    })
-    // console.log(state.spotifyData);
-}
+// function renderArtistImages() {
+//     state.spotifyData.forEach(function (item, index) {
+//         var html = htmlArtistImg(item, index);
+//         $(".img-container").eq(index).html(html);
+//     })
+//     // console.log(state.spotifyData);
+// }
 
 function watchSubmit() {
     $('.js-search-form').submit(function (e) {
         e.preventDefault();
         var query = $(this).find('.js-query').val();
         state.query = query;
-        renderSimilarArtist();
+        renderSimilarArtists();
     });
 }
 
-function htmlTemplate(state, index) {
+function htmlTemplate(artist, index) {
+    var img = "";
+    if (artist.Thumbnail) {
+        img = htmlArtistImg(artist, index);
+        console.debug('here');
+    }
     var html = '<div class="row main-container">' +
         '<div class="col-8">' +
         '<div class="artist-container">' +
-        '<h3>' + state.Name + '</h3>' +
-        '<p>' + state.wTeaser + '</p>' +
+        '<h3>' + artist.Name + '</h3>' +
+        '<p>' + artist.wTeaser + '</p>' +
         // '<button class="js-play-artist play-artist">play album</button>' +
         '</div>' +
         '</div>' +
-        '<div class="col-4">' +
-        '<div class="img-container">' +
-
-        '</div>' +
+        '<div class="col-4">' + img +
         '</div>' +
         '</div>'
 
@@ -105,13 +119,17 @@ function htmlTemplate(state, index) {
 }
 
 function htmlArtistImg(state, index) {
-    var html = '<img class="artist-img" src="' +
-        state.artists.items[0].images[0].url + '" alt="placeholder">' +
+    //TODO: Use Thumbnail and ArtistId
+    console.debug(state);
+    var html = '<div class="img-container">' +
+        '<img class="artist-img" src="' +
+        state.Thumbnail[0].url + '" alt="placeholder">' +
         '<iframe src="https://open.spotify.com/embed?uri=spotify:' +
-        'artist:' + state.artists.items[0].id + '"' +
+        'artist:' + state.ArtistId + '"' +
         'width="300" height="80" frameborder="0"' +
         'allowtransparency="true">' +
-        '</iframe >';
+        '</iframe >' +
+        '</div>';
     return html;
 }
 
